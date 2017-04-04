5e7957b6a47ede02caf273517fcad3661d5ddca6
Florion <cflorion@gmail.com>
2017-04-05 01:01:34 +0200

    Changes

--- installConfig.sh
+++ installConfig.sh
@@ -70,26 +70,27 @@ mas install 714196447 # MenuBar Stats
 # Config
 #
 
+## FINDER
+
+# Affichage de la bibliothèque
+chflags nohidden ~/Library
+
+# Show path in Finder
+defaults write com.apple.finder ShowPathbar -bool true
+
 # Install Command Line Tools without Xcode
 xcode-select --install
 
-# Set Auto Show/Hide Delay
-# The float number defines the show/hide delay in ms.
-defaults write com.apple.dock autohide -bool true
-defaults write com.apple.Dock autohide-delay -float 0
+# Finder displays files and folders in column view
+defaults write com.apple.Finder FXPreferredViewStyle clmv
 
-# Autohide dock without animation
-defaults write com.apple.dock autohide-time-modifier -float 0; killall Dock
-killall Dock
+# Pas de création de fichiers .DS_STORE
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
-# Key Repeat
-# Disable the default "press and hold" behavior.
-defaults write -g ApplePressAndHoldEnabled -bool false
-
-# Key Repeat Rate
-# Sets a very fast repeat rate, adjust to taste.
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
-defaults write NSGlobalDomain KeyRepeat -int 2
+# Mot de passe demandé immédiatement quand l'économiseur d'écran s'active
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
 
 # Create workspace directory
 mkdir ~/workspace
@@ -108,5 +109,37 @@ defaults write com.apple.dock mru-spaces -bool false
 defaults write com.apple.dock wvous-tl-corner -int 2
 defaults write com.apple.dock wvous-tr-corner -int 4
 
-# Reboot
+# Set Auto Show/Hide Delay
+# The float number defines the show/hide delay in ms.
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.Dock autohide-delay -float 0
+
+# Autohide dock without animation
+defaults write com.apple.dock autohide-time-modifier -float 0; killall Dock
+
+## CLAVIER ET TRACKPAD
+
+# Accès au clavier complet (tabulation dans les boîtes de dialogue)
+sudo defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+# Key Repeat
+# Disable the default "press and hold" behavior.
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Key Repeat Rate
+# Sets a very fast repeat rate, adjust to taste.
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
+
+# Réglages Trackpad : toucher pour cliquer
+sudo defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+sudo defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Cleanup
+brew cleanup
+rm -f -r /Library/Caches/Homebrew/*
+
+# End
+killall Dock
+killall Finder
 sudo reboot
